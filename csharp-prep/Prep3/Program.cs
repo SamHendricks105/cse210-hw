91deaab5f553b545823603e7cb2bf9808fa7f881
Samhendricks105 <samhendricks360@gmail.com>
2023-01-06 19:30:18 -0700

    completed prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Asks user for a magic number for the game
+        Console.Write("What is the Magic Number?: ");
+        string magic = Console.ReadLine();
+        //Now ask for the guess
+        Console.Write("What is your guess?: ");
+        string guess = Console.ReadLine();
+        // the win varible will keep track of wether the game is won or not.
+        bool win = false;
+        
+        while (win == false)
+        {
+            if (guess == magic)
+            {
+                win =true;
+            }
+            else
+            {
+                Console.WriteLine("Nope not it.");
+                Console.WriteLine("");
+                Console.Write("What is your guess?: ");
+                guess = Console.ReadLine();
+            }
+        }
+        Console.WriteLine("");
+        Console.WriteLine("Wow you just guessed your own number.");
+        Console.WriteLine("I am so proud of you.");
     }
 }
