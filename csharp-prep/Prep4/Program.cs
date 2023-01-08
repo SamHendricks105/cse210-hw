f5f8d9530f36ad6eaf5d9c222b873c92aa93831a
Samhendricks105 <samhendricks360@gmail.com>
2023-01-07 23:23:20 -0700

    Made changes

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int num =5;
+        while (num !=0)
+        {
+        Console.Write("Enter a number:");
+        string sNum = Console.ReadLine();
+        num = int.Parse(sNum);
+        }
+
     }
 }
