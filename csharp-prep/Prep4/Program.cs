1dd39d55f7e1dd593843c157785de6c89ccdd3a2
Samhendricks105 <samhendricks360@gmail.com>
2023-01-09 12:47:13 -0700

    finsihed prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,10 +1,10 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        List<int> numbers;
+        List<int> numbers= new List<int>();
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         int num =5;
         while (num !=0)
@@ -12,7 +12,21 @@ class Program
         Console.Write("Enter a number:");
         string sNum = Console.ReadLine();
         num = int.Parse(sNum);
+        numbers.Add(num);
         }
-
+        int sum =0;
+        int great=0;
+        foreach (int n in numbers)
+        {
+            sum += n;
+            if (great < n)
+            {
+                great =n;
+            }
+            
+        }
+        Console.WriteLine($"The sum total is:{sum}");
+        Console.WriteLine($"The Average is:{sum/numbers.Count}");
+        Console.WriteLine($"The largest number is:{great}");
     }
 }
