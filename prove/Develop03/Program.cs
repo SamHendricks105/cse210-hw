935bfc4a28893f34aa58c6b1012bedb6b034de93
Samhendricks105 <samhendricks360@gmail.com>
2023-01-24 22:52:13 -0700

    The Program runs and ends on quit. does not end on all words gone.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,9 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-        
-        
+        string i ="";
+       string getInput()
+        {
+            string input;
            
+            input = Console.ReadLine();
+            while (input != "" & input !="quit" & input !="Quit")
+            {
+                Console.WriteLine("Invalid Choice Valid options are quit or pressing enter");
+                input = Console.ReadLine();
+            }
+            
+            return input;
+        }
+        
+        //i = getInput();
+        string text = "5  Trust in the Lord with all thine heart;";
+
+        Reference scriptureReference = new Reference("Proverbs", "3", "5-6");
+        Scripture scripture = new Scripture(scriptureReference, text); 
+        Memorizer scriptureMem = new Memorizer(scripture);
+        
+        
+        
+        while (scriptureMem.hasWordsLeft()== true && i != "quit")
+        {
+            Console.Clear();
+            Console.WriteLine(string.Format("{0} {1}", scriptureReference.toString(),scriptureMem.toString()));
+            i =getInput();            
+            scriptureMem.removeWordsFromList();
+        }
     }
 }
